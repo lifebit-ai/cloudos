ab47e746e269cafe8bbe0f4e816f9fa24b86fa27
Sangram Keshari Sahu <sangramsahu15@gmail.com>
2020-07-27 21:03:53 +0530

    Adds more necessary files and R-CMD check

--- R/extractCohort.R
+++ R/extractCohort.R
@@ -13,7 +13,7 @@
 #'
 #' @return output
 #'
-#' @examples extractCohort(baseUrl = "https://ukb-dev.lifebit.ai", apikey = "hEHFlgtRTIcL8Rqu0gb9HKTTPWWtsfDppEF6xZf7", teamId = "5e91151f75edb766bf8fba26", pageNumber = 0, pageSize = 10)
+# @examples extractCohort(baseUrl = "https://ukb-dev.lifebit.ai", apikey = "hEHFlgtRTIcL8Rqu0gb9HKTTPWWtsfDppEF6xZf7", teamId = "5e91151f75edb766bf8fba26", pageNumber = 0, pageSize = 10)
 #'
 #' @export extractCohort
 #'
