3032cbb0f10fe13fdf1aaf43fffe10a3de5ee992
Sangram Keshari Sahu <sangramsahu15@gmail.com>
2020-08-07 15:58:40 +0530

    fixes output and doc

--- R/extract_participants.R
+++ R/extract_participants.R
@@ -2,33 +2,30 @@
 #'
 #' @description Extracts selected participants.
 #'
-#' @param baseurl Base URL of the CloudOS server. Required
-#' @param auth  An authontication token. Example - Bearer token. Required
-#' @param apikey A API key. Required if auth not provided.
-#' @param raw_data A JSON string for selected participants.
+#' @param baseurl Base URL of the CloudOS server. (Required)
+#' @param auth  An authentication method. (Required)
+#' Example - Bearer token or API key.
+#' @param raw_data A JSON string for selected participants. (Required)
 #'
-#' @return Ideally a dataframe.
+#' @return A dataframe.
 #'
-# @examples 
-# extract_participants(baseurl= "https://cloudos.lifebit.ai", 
-#              auth = "Bearer ***token***",
-#              raw_data = "a JSON string for selected participants")
-#'
+#' @examples
+#' \dontrun{
+#' extract_participants(baseurl= "https://cloudos.lifebit.ai",
+#'              auth = "Bearer ***token***",
+#'              raw_data = "a JSON string for selected participants")
+#' }
 #' @export
-extract_participants <- function(baseurl, 
-                                auth, 
-                                apikey,
-                                raw_data){
-  url = paste(baseurl,"api/v1/cohort/participants/export", sep="/")
-  r <- httr::POST(url, 
-                  httr::add_headers(.headers = c('Authorization' = auth,
-                                           'accept' = '*/*',
-                                           'content-type' = 'application/json')), 
+extract_participants <- function(baseurl, auth, raw_data) {
+  url <- paste(baseurl, "api/v1/cohort/participants/export", sep = "/")
+  r <- httr::POST(url,
+                  httr::add_headers(.headers = c("Authorization" = auth,
+                                           "accept" = "*/*",
+                                      "content-type" = "application/json")),
                   body = raw_data,
                   encode = "json"
         )
-  res <- httr::content(r)
-  res_list <- xml2::as_list(res)
-  table <- res_list$html
-  return(table)
+  res <- httr::content(r, as = "text")
+  df <- utils::read.csv(textConnection(res))
+  return(df)
 }
---
+++ tests/testthat/test-extract.R
@@ -0,0 +1,3 @@
+test_that("dummy test - length computed correctly", {
+  expect_success(expect_length(1, 1))
+})
