b324f8a25852a361fcfe5375f1aef929ddaadf5a
Sangram K Sahu <sangram@lifebit.ai>
2020-08-07 17:41:14 +0530

    fixes output and doc (#4)

--- R/list_cohorts.R
+++ R/list_cohorts.R
@@ -2,30 +2,30 @@
 #'
 #' @description Extracts the data frame with limited cohort data columns.
 #'
-#' @param baseurl Base URL of the CloudOS server. Required
-#' @param auth  An authontication token. Example - Bearer token. Required
-#' @param apikey A API key. Required if auth not provided.
-#' @param teamid Team ID in CloudOS account. Required parameter
-#' @param page_number Number of page. Optional. Default - 0
-#' @param page_size Number of entries in a page. Optional. Default - 10
+#' @param baseurl Base URL of the CloudOS server. (Required)
+#' @param auth  An authentication method. (Required)
+#' Example - Bearer token or API key.
+#' @param teamid Team ID in CloudOS account. (Required)
+#' @param page_number Number of page. (Optional) Default - 0
+#' @param page_size Number of entries in a page. (Optional) Default - 10
 #'
 #' @return A list.
 #'
-# @examples 
-# list_cohorts(baseurl= "https://cloudos.lifebit.ai", 
-#              auth = "Bearer ***token***",
-#              teamid = "***teamid***")
-#'
+#' @examples
+#' \dontrun{
+#' list_cohorts(baseurl= "https://cloudos.lifebit.ai",
+#'              auth = "Bearer ***token***",
+#'              teamid = "***teamid***")
+#' }
 #' @export
-list_cohorts <- function(baseurl, 
-                         auth, 
-                         apikey,
-                         teamid, 
-                         page_number = 0, 
-                         page_size = 10 ){
-  url = paste(baseurl,"api/v1/cohort", sep="/")
-  r <- httr::GET(url, 
-                 httr::add_headers(Authorization = auth), 
+list_cohorts <- function(baseurl,
+                         auth,
+                         teamid,
+                         page_number = 0,
+                         page_size = 10) {
+  url <- paste(baseurl, "api/v1/cohort", sep = "/")
+  r <- httr::GET(url,
+                 httr::add_headers(Authorization = auth),
                  query = list(teamId = teamid))
   res <- httr::content(r)
   cohort <- res$cohort
