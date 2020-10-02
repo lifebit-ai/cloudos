aa77d597a5e016a51aab702522fbca4884b9713b
Sangram Keshari Sahu <sangramsahu15@gmail.com>
2020-10-02 18:08:34 +0530

    removes docs folder (pkgdown renders) as this is will auto generate in gh-pages branch

--- docs/reference/figures/logo/hexSticker.R
+++
@@ -1,8 +0,0 @@
-library(hexSticker)
-
-imgurl <- "logo/cloudos_logo_partial.png"
-sticker(imgurl, package = "CloudOS",
-        p_size=25, p_color = "#2285c7", p_x = 1, p_y = 0.6,
-        s_x=1, s_y=1.2, s_width=.6,
-        h_fill="#FFFFFF", h_color="#2285c7",
-        filename="logo/hexlogo.png")
