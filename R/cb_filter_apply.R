ec5435b20e3511dbf9cbbb523b080821339d2dda
Sangram K Sahu <sangram@lifebit.ai>
2020-11-26 22:34:34 +0530

    Adds options to add columns from samples table (#25)
    * apply user define column id and also now it can keep existing columns and filters
    
    * doc update

--- R/cb_filter_apply.R
+++ R/cb_filter_apply.R
@@ -29,9 +29,58 @@
       }
     filter_list_all <- c(filter_list_all, filter_list)
   }
+  return(filter_list_all)
+}
+
+.get_existing_filter_body <- function(my_cohort_info) {
   
-  filter_body <- list("moreFilters" = filter_list_all)
-  return(filter_body)
+  filter_list_all <- c()
+  for(n in 1:length(my_cohort_info$moreFields)){
+    more_fields <- my_cohort_info$moreFields[[n]]
+    filter_id <- more_fields$fieldId
+    if("range" %in% names(more_fields)){
+      #print("range")
+      filter_list <- list(list("fieldId" = jsonlite::unbox(more_fields$fieldId),
+                               "instance" = c(0),
+                               "range" = list("from" = jsonlite::unbox(more_fields$range$from),
+                                              "to" = jsonlite::unbox(more_fields$range$to)
+                               )))
+    }
+    if("value" %in% names(more_fields)){
+      #print("value")
+      filter_list <- list(list("fieldId" = jsonlite::unbox(more_fields$fieldId),
+                               "instance" = c(0),
+                               "value" = unlist(more_fields$value)
+      ))
+    }
+    filter_list_all <- c(filter_list_all, filter_list)
+  }
+  return(filter_list_all)
+}
+
+.build_column_body <- function(column_ids) {
+  
+  column_body_all <- c()
+  for(i in 1:length(column_ids)){
+    column_body_temp <- list("id" = jsonlite::unbox(column_ids[i]),
+                             "instance" = jsonlite::unbox(0),
+                             "array" = list("type" = jsonlite::unbox("exact"),
+                                            "value" = jsonlite::unbox(0)))
+    column_body_all <- c(column_body_all, list(column_body_temp))
+  }
+  return(column_body_all)
+}
+
+.get_existing_columns_body <- function(my_cohort_info){
+  column_body_all <- c()
+  for(i in length(my_cohort_info$columns)){
+    column_body_temp <- list("id" = jsonlite::unbox(my_cohort_info$columns[[i]]$id),
+                             "instance" = jsonlite::unbox(my_cohort_info$columns[[i]]$instance),
+                             "array" = list("type" = jsonlite::unbox(my_cohort_info$columns[[i]]$array$type),
+                                            "value" = jsonlite::unbox(my_cohort_info$columns[[i]]$array$value)))
+    column_body_all <- c(column_body_all, list(column_body_temp))
+  }
+  return(column_body_all)
 }
 
 #' @title Apply a Filter
@@ -43,6 +92,9 @@
 #' @param cohort A cohort object. (Required)
 #' See constructor function \code{\link{cb_create_cohort}} or \code{\link{cb_load_cohort}}
 #' @param filter_query Phenotypic filter query. 
+#' @param column_ids Filter IDs as column in the table
+#' @param keep_existing_filter If wants to keep existing filters (Default: TRUE)
+#' @param keep_existing_columns If wants to keep existing columns (Default: TRUE)
 #' 
 #' @return A confirmation string.
 #' 
@@ -53,9 +105,39 @@
 #'                filter_query = list("22" = list("from" = "2015-05-13", "to" = "2016-04-29"), "50" = c("Father", "Mother")))
 #'
 #' @export
-cb_apply_filter <- function(cloudos, cohort, filter_query) {
+cb_apply_filter <- function(cloudos, 
+                            cohort, 
+                            filter_query,
+                            column_ids,
+                            keep_existing_filter = TRUE,
+                            keep_existing_columns = TRUE) {
+  
+  # get info about existing columns and fields
+  my_cohort_info <- .get_cohort_info(cloudos = cloudos, cohort_id = cohort_id)
+  
+  # cohort columns
+  all_columns <- c()
+  if(!missing(column_ids)){
+    all_columns <- .build_column_body(column_ids)
+  }
+  if(keep_existing_columns){
+    existing_columns <- .get_existing_columns_body(my_cohort_info)
+    all_columns <- c(existing_columns, all_columns)
+  }
+  
+  # cohort filters
+  all_filters <- c()
+  if(!missing(filter_query)){
+    all_filters <- .build_filter_body(filter_query)
+  }
+  if(keep_existing_filter){
+    existing_filters <- .get_existing_filter_body(my_cohort_info)
+    all_filters <- c(existing_filters, all_filters)
+  }
+  
   # prepare request body
-  r_body <- .build_filter_body(filter_query)
+  r_body <- list("columns" = all_columns,
+                 "moreFilters" = all_filters)
 
   # make request
   url <- paste(cloudos@base_url, "v1/cohort", cohort@id, "filters", sep = "/")
@@ -92,7 +174,7 @@ cb_apply_filter <- function(cloudos, cohort, filter_query) {
 #' @export
 cb_apply_filter_dry_run <- function(cloudos, cohort, filter_query) {
   # prepare request body
-  r_body <- .build_filter_body(filter_query)
+  r_body <- list("moreFilters" = .build_filter_body(filter_query))
 
   # add additional content
   r_body[["ids"]] = list()
