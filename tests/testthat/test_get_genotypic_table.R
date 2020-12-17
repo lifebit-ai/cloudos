42639aef564b5749a2fca16167e40584e7df2ae1
Sangram Keshari Sahu <sangramsahu15@gmail.com>
2020-12-17 21:23:39 +0530

    unit test case update

--- tests/testthat/test_get_genotypic_table.R
+++ tests/testthat/test_get_genotypic_table.R
@@ -1,8 +1,8 @@
 # capture_requests({
 #   # set a dummy cloudos credentials
-#   my_cloudos <- connect_cloudos(base_url = "http://cohort-browser-766010452.eu-west-1.elb.amazonaws.com/api/",
-#                         auth = Sys.getenv("test_cloudos_apikey"),
-#                         team_id = "5f046bf6c132dd15fdd1a525")
+#   my_cloudos <- connect_cloudos(base_url = "http://cohort-browser-dev-110043291.eu-west-1.elb.amazonaws.com/api",
+#                                 auth = Sys.getenv("test_cloudos_apikey"),
+#                                 team_id = Sys.getenv("test_cloudos_team_id"))
 #   # test
 #   genotypic_table <- cb_get_genotypic_table(my_cloudos)
 #   expect_s3_class(genotypic_table, "data.frame")
@@ -12,9 +12,9 @@ with_mock_api({
   test_that("Get a genotypic table", {
     
     # set a dummy cloudos credentials
-    my_cloudos <- connect_cloudos(base_url = "http://cohort-browser-766010452.eu-west-1.elb.amazonaws.com/api/",
+    my_cloudos <- connect_cloudos(base_url = "http://cohort-browser-dev-110043291.eu-west-1.elb.amazonaws.com/api",
                           auth = "apikey",
-                          team_id = "5f046bf6c132dd15fdd1a525")
+                          team_id = "5f7c8696d6ea46288645a89f")
     # test
     genotypic_table <- cb_get_genotypic_table(my_cloudos)
     expect_s3_class(genotypic_table, "data.frame")
