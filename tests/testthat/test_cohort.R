1c48c05fb9ab403a65677050292508cae7bcea60
ilevantis <ilevantis@users.noreply.github.com>
2021-07-08 16:48:53 +0100

    specify cb_version v1 in tests

--- tests/testthat/test_cohort.R
+++ tests/testthat/test_cohort.R
@@ -5,7 +5,7 @@
 
 with_mock_api({
   test_that("Get a cohort class method", {
-    my_cohort <- cb_load_cohort(cohort_id = "5f9af3793dd2dc6091cd17cd")
+    my_cohort <- cb_load_cohort(cohort_id = "5f9af3793dd2dc6091cd17cd", cb_version = "v1")
     expect_s4_class(my_cohort, "cohort")
   })
 })
