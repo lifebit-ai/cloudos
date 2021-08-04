015550aa274eb9ac000377b8c1e0853e88c77242
ilevantis <ilevantis@users.noreply.github.com>
2021-08-04 13:56:04 +0100

    Fix non-portable file paths in mock api tests (#60)
    * changed api paths in tests to reduce path lengths
    
    * explain mock api test generation procedure

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,20 @@
 library(testthat)
 library(cloudos)
 
-Sys.setenv(CLOUDOS_BASEURL = "http://cohort-browser-dev-110043291.eu-west-1.elb.amazonaws.com/api")
+Sys.setenv(CLOUDOS_BASEURL = "api")
 Sys.setenv(CLOUDOS_TEAMID = "5f7c8696d6ea46288645a89f")
 Sys.setenv(CLOUDOS_TOKEN = "no real")
 
 test_check("cloudos")
+
+# CRAN requires path lengths within the package to be <100 chars long so they can be unpacked
+# from a tarball correctly. This means the following for creating mock API tests with httptest:
+# + Run the code that will comprise your test using a real API server and surround the code
+#   with capture_requests({ ... }).
+# + This will create a directory structure that matches the server url you used.
+# + Now copy the 'v1' or 'v2' directory nested in that structure into tests/testthat/api/
+# + Check to see if any of the .json or .R files generated in that structure have the cohort id
+#   that was used in the code as part of their filename.
+# + If so, change that part of the filename to f.e.g. "05" and edit the unit test code to use
+#   this new short cohort id.
+# + Add your new unit test code in the tests/testthat directory.
