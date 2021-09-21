d6953b98cd9799a183ad7c037a446fc3624e209b
Henry Morgan Stewart <henry@Henrys-MBP.broadband>
2021-09-21 11:58:43 +0100

    s3

--- R/cb_class.R
+++ R/cb_class.R
@@ -176,3 +176,9 @@ setMethod("show", "cohort",
             cat("Cohort Browser version: ", object@cb_version, "\n")
           }
 )
+
+# register methods
+S3method("!",cbQuery)
+S3method("&",cbQuery)
+S3method("|",cbQuery)
+
