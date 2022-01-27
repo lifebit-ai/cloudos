983ee7755f34bb798ed1a9d8bfc180e4967296eb
ilevantis <ilevantis@users.noreply.github.com>
2022-01-27 11:34:59 +0000

    Add more information to cohort class (#79)
    * add num_participants slot and query_pheontype_ids slot
    * updated mock api files and tests

--- tests/testthat/test_cohort.R
+++ tests/testthat/test_cohort.R
@@ -1,11 +1,11 @@
 # capture_requests({
-#   my_cohort <- cb_load_cohort(cohort_id = "5f9af3793dd2dc6091cd17cd")
+#   my_cohort <- cb_load_cohort(cohort_id = "61f18422acdea8299c3f56b5", cb_version = "v1")
 #   expect_s4_class(my_cohort, "cohort")
 # })
 
 with_mock_api({
   test_that("Get a cohort class method", {
-    my_cohort <- cb_load_cohort(cohort_id = "01", cb_version = "v1")
+    my_cohort <- cb_load_cohort(cohort_id = "61f18422acdea8299c3f56b5", cb_version = "v1")
     expect_s4_class(my_cohort, "cohort")
   })
 })
